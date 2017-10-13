f7d8e970530f166bdab91c7fed1aae7189fc0635
ivan <ivan@ivanplex.com>
2017-10-13 13:20:07 +0100

    Updated App ID

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -48,7 +48,7 @@
 
   window.fbAsyncInit = function() {
   FB.init({
-    appId      : '{your-app-id}',
+    appId      : '{281820072245494}',
     cookie     : true,  // enable cookies to allow the server to access 
                         // the session
     xfbml      : true,  // parse social plugins on this page
